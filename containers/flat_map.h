ee46bc86737a2dbb7c91967aa4f4d5997ffb97bb
AndreaTgc <andrea@MacBook-Air-di-Andrea.local>
2025-04-22 21:01:21 +0200

    flat_map rehashing

--- containers/flat_map.h
+++ containers/flat_map.h
@@ -51,70 +51,93 @@ _lc_join(__map_t, fib_hash)(uint64_t hash, size_t cap) {
 }
 
 static inline void
-_lc_join(__map_t, init)(__map_t *map, size_t cap, uint64_t (*hash)(K),
+_lc_join(__map_t, init)(__map_t *self, size_t cap, uint64_t (*hash)(K),
                         bool (*eq)(K, K), void (*dk)(K), void (*dv)(V)) {
-  ASSERT((map != NULL), "Trying to call init on a NULL map\n");
+  ASSERT((self != NULL), "Trying to call init on a NULL map\n");
   ASSERT((hash != NULL), "Trying to initialise a map without a hash func\n");
   if (cap != 0) {
     ASSERT(((cap & cap - 1) == 0),
            "The provided capacity must be a power of 2");
   }
-  memset(map, 0, sizeof(*map));
-  map->capacity = cap == 0 ? 32 : cap;
-  map->hash = hash;
-  map->key_eq = eq ? eq : _lc_join(__map_t, default_key_eq);
-  map->drop_k = dk;
-  map->drop_v = dv;
-  map->slots = (__entry_t *)calloc(map->capacity, sizeof(__entry_t));
+  memset(self, 0, sizeof(*self));
+  self->capacity = cap == 0 ? 32 : cap;
+  self->hash = hash;
+  self->key_eq = eq ? eq : _lc_join(__map_t, default_key_eq);
+  self->drop_k = dk;
+  self->drop_v = dv;
+  self->slots = (__entry_t *)calloc(self->capacity, sizeof(__entry_t));
 }
 
 static inline V *
-_lc_join(__map_t, find)(__map_t *map, K key) {
-  size_t index = _lc_join(__map_t, fib_hash)(map->hash(key), map->capacity);
+_lc_join(__map_t, find)(__map_t *self, K key) {
+  size_t index = _lc_join(__map_t, fib_hash)(self->hash(key), self->capacity);
   uint8_t dist = 0;
   for (;;) {
-    __entry_t *slot = &map->slots[index];
+    __entry_t *slot = &self->slots[index];
     if (!slot->in_use)
       return NULL;
     if (slot->off < dist)
       return NULL;
-    if (map->key_eq(slot->key, key))
+    if (self->key_eq(slot->key, key))
       return &slot->value;
-    index = (index + 1) & (map->capacity - 1);
+    index = (index + 1) & (self->capacity - 1);
   }
   return NULL;
 }
 
 static inline bool
-_lc_join(__map_t, contains)(__map_t *map, K key) {
-  return _lc_join(__map_t, find)(map, key) != NULL;
+_lc_join(__map_t, contains)(__map_t *self, K key) {
+  return _lc_join(__map_t, find)(self, key) != NULL;
 }
 
 static inline bool
-_lc_join(__map_t, get)(__map_t *map, K key, V *out) {
-  V *t = _lc_join(__map_t, find)(map, key);
+_lc_join(__map_t, get)(__map_t *self, K key, V *out) {
+  V *t = _lc_join(__map_t, find)(self, key);
   if (!t)
     return false;
   *out = *t;
   return true;
 }
 
+// Declaration for rehashing
+static inline bool _lc_join(__map_t, insert)(__map_t *self, K key, V value);
+
+static inline void
+_lc_join(__map_t, rehash)(__map_t *self, size_t new_cap) {
+  ASSERT((new_cap > self->capacity),
+         "Trying to resize the table with a smaller capacity\n");
+  __entry_t *new_slots = (__entry_t *)calloc(new_cap, sizeof(__entry_t));
+  __entry_t *old_slots = self->slots;
+  size_t old_cap = self->capacity;
+  self->capacity = new_cap;
+  self->slots = new_slots;
+  for (size_t i = 0; i < old_cap; i++) {
+    if (old_slots[i].in_use) {
+      bool res = _lc_join(__map_t, insert)(self, old_slots[i].key, old_slots[i].value);
+      ASSERT((res), "Something went wrong during rehashing\n");
+    }
+  }
+}
+
 static inline bool
-_lc_join(__map_t, insert)(__map_t *map, K key, V value) {
-  size_t index = _lc_join(__map_t, fib_hash)(map->hash(key), map->capacity);
+_lc_join(__map_t, insert)(__map_t *self, K key, V value) {
+  if(self->size == self->capacity) {
+      _lc_join(__map_t, rehash)(self, self->capacity << 1);
+  }
+  size_t index = _lc_join(__map_t, fib_hash)(self->hash(key), self->capacity);
   uint8_t dist = 0;
   __entry_t new_entry = {key, value, true, dist};
   for (;;) {
-    __entry_t *slot = &map->slots[index];
+    __entry_t *slot = &self->slots[index];
     if (!slot->in_use) {
       // Found an empty slot, we use it for insertion
       *slot = new_entry;
-      map->size++;
+      self->size++;
       return true;
     }
     // Check if the key is already in the map
     // if yes, abort the insertion
-    if (map->key_eq(slot->key, key))
+    if (self->key_eq(slot->key, key))
       return false;
 
     if (slot->off < dist) {
@@ -126,57 +149,57 @@ _lc_join(__map_t, insert)(__map_t *map, K key, V value) {
 
     dist++;
     new_entry.off = dist;
-    index = (index + 1) & (map->capacity - 1); // capacity is power of 2
+    index = (index + 1) & (self->capacity - 1); // capacity is power of 2
   }
   // We should never get here
   return false;
 }
 
 static inline bool
-_lc_join(__map_t, remove)(__map_t *map, K key) {
-  size_t index = _lc_join(__map_t, fib_hash)(key, map->capacity);
+_lc_join(__map_t, remove)(__map_t *self, K key) {
+  size_t index = _lc_join(__map_t, fib_hash)(key, self->capacity);
   uint8_t dist = 0;
   for (;;) {
-    __entry_t *slot = &map->slots[index];
+    __entry_t *slot = &self->slots[index];
     if (!slot->in_use)
       return false;
     if (slot->off < dist)
       return false;
-    if (map->key_eq(slot->key, key)) {
-      if (map->drop_k)
-        map->drop_k(slot->key);
-      if (map->drop_v)
-        map->drop_v(slot->value);
-      size_t next = (index + 1) & (map->capacity - 1);
-      while (map->slots[next].in_use && map->slots[next].off > 0) {
-        map->slots[index] = map->slots[next];
-        map->slots[index].off--;
+    if (self->key_eq(slot->key, key)) {
+      if (self->drop_k)
+        self->drop_k(slot->key);
+      if (self->drop_v)
+        self->drop_v(slot->value);
+      size_t next = (index + 1) & (self->capacity - 1);
+      while (self->slots[next].in_use && self->slots[next].off > 0) {
+        self->slots[index] = self->slots[next];
+        self->slots[index].off--;
         index = next;
-        next = (next + 1) & (map->capacity - 1);
+        next = (next + 1) & (self->capacity - 1);
         return true;
       }
     }
     dist++;
-    index = (index + 1) & (map->capacity - 1);
+    index = (index + 1) & (self->capacity - 1);
   }
   // We should never get here
   return false;
 }
 
 static inline void
-_lc_join(__map_t, destroy)(__map_t *map) {
-  if (!map)
+_lc_join(__map_t, destroy)(__map_t *self) {
+  if (!self)
     return;
-  if (map->slots) {
-    for (size_t i = 0; i < map->capacity; i++) {
-      if (map->slots[i].in_use) {
-        if (map->drop_k)
-          map->drop_k(map->slots[i].key);
-        if (map->drop_v)
-          map->drop_v(map->slots[i].value);
+  if (self->slots) {
+    for (size_t i = 0; i < self->capacity; i++) {
+      if (self->slots[i].in_use) {
+        if (self->drop_k)
+          self->drop_k(self->slots[i].key);
+        if (self->drop_v)
+          self->drop_v(self->slots[i].value);
       }
     }
-    free(map->slots);
+    free(self->slots);
   }
-  memset(map, 0, sizeof(*map));
+  memset(self, 0, sizeof(*self));
 }
